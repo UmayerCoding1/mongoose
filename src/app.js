972af2a83836aaf0cfa72d06f74a29fb01b86275
umayerCoding1 <umayerpro175@gmail.com>
2024-12-01 00:23:31 +0600

    all setup config

--- src/app.js
+++ src/app.js
@@ -8,12 +8,16 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json({limit: '30kb'}));
+app.use(express.json());
 app.use(express.urlencoded({extended: true, limit: '36kb'}));
 app.use(express.static('public'));  
 app.use(cookieParser());
 
 
 
+// routes import
+import userRouter from './routes/user.routes.js'
 
+// routes declaration
+app.use('/api/users', userRouter)
 export {app};
